ae2e32f5a9961b0e7e27cb53a0180321cfe5fac2
styven <styvenn16@gmail.com>
2024-12-12 10:35:26 -0300

    salvando os ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,5 +1,5 @@
 import {randomUUID} from "node:crypto"
-export function create({request, response}){
+export function create({request, response, database}){
     const {equipment,description,user_name} = request.body
 
     const ticket = {
@@ -11,5 +11,6 @@ export function create({request, response}){
         create_at:new Date(),
         updated_at:new Date(),
     }
-    return response.end(JSON.stringify(ticket));
+    database.insert("tickets", ticket)
+    return response.writeHead(201).end(JSON.stringify(ticket));
 }
